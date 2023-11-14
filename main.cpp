f7cef63bdab2cf6ffc2f3da27f3ac86587081b56
Justin Lin <jchlin06@gmail.com>
2023-11-13 19:11:19 -0500

    Menu

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+    int placeholdervalue;
+    std::cout << "How many nodes are in graph?" << std::endl;
+    std::cin >> placeholdervalue;
+    std::cout << "Press 1 to add an edge to graph" <<std::endl;
+    std::cout << "Press 2 to remove an edge from graph" <<std::endl;
+    std::cout << "Press 3 to find an edge in the graph" <<std::endl;
+    std::cout << "Press 4 to find the out edges of a graph" <<std::endl;
+    std::cout << "Press 5 to find the in edges of a vertices" <<std::endl;
+    std::cout << "Press 6 to print out adjacency matrix" <<std::endl;
+    std::cout << "Press 7 to quit" <<std::endl;
+switch (placeholdervalue)
+{
+case 1:
+    /* code */
+    break;
+case 2:
+
+    break;
+
+case 3:
+
+    break;
+
+case 4:
+
+    break;
+
+case 5:
+
+    break;
+
+case 6:
+
+    break;
+
+case 7:
+
+    break;
+        
+default:
+    break;
+}
+}
